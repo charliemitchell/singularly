5b1e78261f8e5579d14d268a2fc69543a9a347de
Charlie Mitchell <charliesemailis@gmail.com>
2023-08-27 12:13:36 -0700

    update coverage

--- src/errors.js
+++ src/errors.js
@@ -1,13 +1,29 @@
+const { rollback, fail } = require("./symbols")
+
 module.exports.FailedContextError = class FailedContextError extends Error {
   constructor(message) {
     super(message);
     this.name = "FailedContextError";
   }
-}
+};
 
 module.exports.RollbackError = class RollbackError extends Error {
   constructor(message) {
     super(message);
     this.name = "RollbackError";
   }
-}
+};
+
+module.exports.catchInteractorError = async function (err, context) {
+  await context[rollback]();
+
+  if (err.name != "FailedContextError") {
+    context[fail](err);
+  }
+};
+
+module.exports.catchOrganizerError = async function (err, context) {
+  if (err.name != "FailedContextError") {
+    context[fail](err);
+  }
+};
